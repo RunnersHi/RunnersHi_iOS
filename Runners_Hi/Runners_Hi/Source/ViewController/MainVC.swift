4232497c57e2afdee02f3879ae5ea8ed7af8a59d
minhee <xwoud@naver.com>
2020-07-05 21:46:06 +0900

    <Update> MainVC autolayout 잡기

--- Runners_Hi/Runners_Hi/Source/ViewController/MainVC.swift
+++ Runners_Hi/Runners_Hi/Source/ViewController/MainVC.swift
@@ -9,22 +9,21 @@
 import UIKit
 
 class MainVC: UIViewController {
-
+    
+    @IBOutlet weak var logoImg: UIImageView!
+    @IBOutlet weak var runBtn: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        mainBasicLayout()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func mainBasicLayout() {
+        //logoImg.image = UIImage()
+        //runBtn.backgroundColor = UIColor()
+        runBtn.setTitle("RUN NOW", for: .normal)
     }
-    */
+    
 
 }
