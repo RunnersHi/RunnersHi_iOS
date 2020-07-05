7e4d1800cd0e47a7391e4396447f2c4b8821763e
minhee <xwoud@naver.com>
2020-07-06 03:15:44 +0900

    <Add> Tabbar Storyboard/VC 생성

---
+++ Runners_Hi/Runners_Hi/Source/ViewController/TabbarVC.swift
@@ -0,0 +1,29 @@
+//
+//  TabBarVC.swift
+//  Runners_Hi
+//
+//  Created by 김민희 on 2020/07/06.
+//  Copyright © 2020 김민희. All rights reserved.
+//
+
+import UIKit
+
+class TabbarVC: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    func setTabBar() {
+        let runningTabStoryboard = UIStoryboard.init(name: "RunningTab", bundle: nil)
+        
+        guard let firstTab = runningTabStoryboard.instantiateViewController(identifier: "MainVC") as MainVC else {
+            return
+        }
+        
+    }
+
+}
