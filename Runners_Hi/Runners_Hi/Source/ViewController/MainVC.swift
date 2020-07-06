29cb1e9dfe1717c3c3b75c06018f0628a5aa8d79
minhee <xwoud@naver.com>
2020-07-07 01:44:19 +0900

    <Update> MainVC label Font,size 설정

--- Runners_Hi/Runners_Hi/Source/ViewController/MainVC.swift
+++ Runners_Hi/Runners_Hi/Source/ViewController/MainVC.swift
@@ -11,9 +11,14 @@ import UIKit
 class MainVC: UIViewController {
     static let identifier: String = "MainVC"
     
+    
+    @IBOutlet weak var mentLabel1: UILabel!
+    @IBOutlet weak var mentLabel2: UILabel!
+    @IBOutlet weak var mentLabel3: UILabel!
     @IBOutlet weak var logoImg: UIImageView!
     @IBOutlet weak var secondView: UIView!
     @IBOutlet weak var runButton: UIButton!
+    
     @IBAction func runButtonDidTap(_ sender: Any) {
         //버튼 클릭시 기능 구현
         
@@ -34,6 +39,10 @@ class MainVC: UIViewController {
         runButton.setTitle("RUN NOW", for: .normal)
         runButton.setTitleColor(.white, for: .normal)
         runButton.layer.cornerRadius = 12
+        mentLabel1.font = UIFont(name: "NanumSquareB", size: 20)
+        mentLabel2.font = UIFont(name: "NanumSquareB", size: 20)
+        mentLabel3.font = UIFont(name: "NanumSquareB", size: 20)
+        runButton.titleLabel?.font = UIFont(name: "AvenirNext-BoldItalic", size: 30)
     }
     
 
