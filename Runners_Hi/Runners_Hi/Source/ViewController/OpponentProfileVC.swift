3199565a07a53b9d5cb36c72f7ccf8e08227572e
minhee <xwoud@naver.com>
2020-07-12 04:19:41 +0900

    <Update> OpponentProfileVC - 코드 정리

--- Runners_Hi/Runners_Hi/Source/ViewController/OpponentProfileVC.swift
+++ Runners_Hi/Runners_Hi/Source/ViewController/OpponentProfileVC.swift
@@ -13,46 +13,51 @@ class OpponentProfileVC: UIViewController {
     
     @IBOutlet weak var questionLabel: UILabel!
     @IBOutlet weak var boxImage: UIImageView!
-    @IBOutlet weak var battleRunnerImage: UIImageView!
-    @IBOutlet weak var battleRunnerIDLabel: UILabel!
+    @IBOutlet weak var battleRunnerImage: UIImageView! //상대 러너 프로필
+    @IBOutlet weak var battleRunnerIDLabel: UILabel! // 상대 러너 아이디
     @IBOutlet weak var scoreView: UIView!
     @IBOutlet weak var levelView: UIView!
     @IBOutlet weak var levelLabel: UILabel!
-    @IBOutlet weak var battleRunnerLevelLabel: UILabel!
-    @IBOutlet weak var scoreLabel: UILabel!
-    @IBOutlet weak var battleRunnerScoreLabel: UILabel!
+    @IBOutlet weak var battleRunnerLevelLabel: UILabel! // 상대 러너 레벨
+    @IBOutlet weak var scoreLabel: UILabel! // 전적
+    @IBOutlet weak var battleRunnerScoreLabel: UILabel! // 상대 러너 전적
     @IBOutlet weak var countLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        basicAutoLayout()
+        setView()
+        setLabel()
         // Do any additional setup after loading the view.
     }
-    private func basicAutoLayout() {
-        view.backgroundColor = UIColor.backgroundgray
+
+}
+
+extension OpponentProfileVC {
+    func setLabel() {
+        
         questionLabel.text = "당신과 함께 뛸 러너는?"
         questionLabel.font = UIFont(name: "NanumSquareB", size: 20)
-        boxImage.image = UIImage(named: "whiteboxMatchsucactivityShadow")
-        levelView.backgroundColor = UIColor.backgroundgray
-        scoreView.backgroundColor = UIColor.backgroundgray
+        
         levelLabel.text = "Lv."
         levelLabel.font = UIFont(name: "NanumSquare", size: 14)
-        battleRunnerLevelLabel.font = UIFont(name: "NanumSquareB", size: 16)
+        
         scoreLabel.text = "전적"
         scoreLabel.font = UIFont(name: "NanumSquare", size: 14)
-        battleRunnerScoreLabel.font = UIFont(name: "NanumSquareB", size: 16)
+        
         countLabel.text = "곧 카운트 다운이 시작됩니다"
         countLabel.font = UIFont(name: "NanumSquareB", size: 16)
+        
+        battleRunnerLevelLabel.font = UIFont(name: "NanumSquareB", size: 16)
+        
+        battleRunnerScoreLabel.font = UIFont(name: "NanumSquareB", size: 16)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func setView() {
+        
+        view.backgroundColor = UIColor.backgroundgray
+        levelView.backgroundColor = UIColor.backgroundgray
+        scoreView.backgroundColor = UIColor.backgroundgray
+        boxImage.image = UIImage(named: "whiteboxMatchsucactivityShadow")
+        
     }
-    */
-
 }
