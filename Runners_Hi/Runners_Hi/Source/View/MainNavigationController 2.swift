14e222d3d0e3b7d26b56622128320a139f42b057
최영재 <brandon94_@naver.com>
2020-07-18 05:00:56 +0900

    <add> 팝업완료후 러닝 상세보기 작업중

---
+++ Runners_Hi/Runners_Hi/Source/View/MainNavigationController 2.swift
@@ -0,0 +1,30 @@
+//
+//  MainNC.swift
+//  Runners_Hi
+//
+//  Created by 김민희 on 2020/07/07.
+//  Copyright © 2020 김민희. All rights reserved.
+//
+
+import UIKit
+
+class MainNavigationController: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
