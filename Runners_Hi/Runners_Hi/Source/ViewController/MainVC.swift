6e31e6e84256fac56057d450cbe2132a6af9d826
xwoud <xwoud@naver.com>
2021-03-25 02:39:53 +0900

    [refact] 코드 정리
    body: 역할에 맡게 코드 위치 변경 및 주석으로 설명 게재
    
    Related to: #13

--- Runners_Hi/Runners_Hi/Source/ViewController/MainVC.swift
+++ Runners_Hi/Runners_Hi/Source/ViewController/MainVC.swift
@@ -13,8 +13,9 @@ class MainVC: UIViewController {
     // MARK: Variable Part
     
     static let identifier: String = "MainVC"
-    var blackView = UIView()
-    private var battleInformation: [BattleInformation] = []
+    
+    var backBlackView = UIView()
+    var battleInformation: [BattleInformation] = []
     
     // MARK: IBOutlet
     
@@ -27,103 +28,124 @@ class MainVC: UIViewController {
     @IBOutlet weak var homeImageView: UIImageView!
     
     @IBOutlet weak var constraint_logo_ment: NSLayoutConstraint!
-    
     @IBOutlet weak var constraint_ment_homeImg: NSLayoutConstraint!
     @IBOutlet weak var constarint_homeImg_button: NSLayoutConstraint!
-   
+    
     
     // MARK: IBAction
     
     @IBAction func runButtonDidTap(_ sender: Any) {
-        blackView.backgroundColor = UIColor.black.withAlphaComponent(0.5)
-        blackView.frame = self.view.frame
-        self.view.addSubview(blackView)
+        // "RUN NOW" 버튼 클릭 시 Action -> 두 종류의 경쟁하기가 나와야함
+        
+        backBlackView.backgroundColor = UIColor.black.withAlphaComponent(0.5)
+        // withAlphaComponent : alpha값이 있는 컬러로 지정
+        backBlackView.frame = self.view.frame
+        self.view.addSubview(backBlackView)
+        // 뒤에 투명한 검은 배경을 깔아줌
+        
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(downPopUpTableView))
+        backBlackView.addGestureRecognizer(tapGesture)
+        // blackView 클릭 시 Action 추가
+        
         UIView.animate(withDuration: 0.3) {
+            // animation 효과로 tableview의 이동을 자연스럽게 만듦
+            
             self.popUpTableView.transform = .identity
-            self.onClickBlackView()
+            // .identity 로 밑으로 숨어있던 popUpTableView를 제자리로 위치시킴
         }
-
-       self.view.bringSubviewToFront(popUpTableView)
-    
+        
+        self.view.bringSubviewToFront(popUpTableView)
+        // black 뷰가 superView에 추가되었기 때문에 popUpTableView를 덮고있는데 그것을 앞으로 끌어내기위해 사용
+        
     }
-
+    
     // MARK: Life Cycle Part
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        basicAutoLayout()
         setbattleInformation()
-        
-        popUpTableView.transform = CGAffineTransform(translationX: 0, y: popUpTableView.frame.height)
+        setText()
+        setView()
     }
-
+    
 }
 
 // MARK: Extension
 
 extension MainVC {
     
-    func onClickBlackView() {
-        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(onGesture))
-
-        blackView.addGestureRecognizer(tapGesture)
-    }
-    
-    @objc func onGesture() {
+    @objc func downPopUpTableView() {
+        // BlackView 클릭 시의 Action
+        
         UIView.animate(withDuration: 0.3) {
+            // animation 효과로 tableview의 이동을 자연스럽게 만듦
+            
             self.popUpTableView.transform = CGAffineTransform(translationX: 0, y: self.popUpTableView.frame.height)
-            //self.setTabBarVisible(visible: true, animated: true)
-            self.blackView.removeFromSuperview()
+            // popUpTableView의 높이만큼 밑으로 내려서 사용자 눈에 보이지 않게 만듦
+            self.backBlackView.removeFromSuperview()
+            // 뒤에 깔려있던 blackView를 화면에서 제거함
         }
     }
     
-    private func setbattleInformation() {
+    func setbattleInformation() {
+        // popUpTableView의 배열 설정
+        
         let date1 = BattleInformation(ment: "다른 러너와 경쟁하기")
         let date2 = BattleInformation(ment: "나의 기록과 경쟁하기")
         
-        battleInformation = [date1,date2]
+        battleInformation.append(contentsOf: [date1,date2])
     }
     
-    private func basicAutoLayout() {
+    // MARK: Function View Style
+    
+    func setView() {
         
         homeImageView.image = UIImage(named: "imgHomeactivityMain")
         logoImageView.image = UIImage(named: "homeLogo")
+        
         self.view.backgroundColor = UIColor.backgroundgray
+        
         insideView.backgroundColor = UIColor.white
         insideView.layer.cornerRadius = 20
+        
+        self.navigationController?.navigationBar.setBackgroundImage(UIImage(), for: UIBarMetrics.default)
+        self.navigationController?.navigationBar.shadowImage = UIImage()
+        self.navigationController?.navigationBar.isTranslucent = true
+        
+        if self.view.frame.height > 700 {
+            // 화면에 길이에 따라 거리 조정 ( 화면 길이가 길면 늘려줌 )
+            
+            constraint_logo_ment.constant = 34
+            constraint_ment_homeImg.constant = 59
+            constarint_homeImg_button.constant = 51
+        }
+        
+        popUpTableView.layer.cornerRadius = 20
+        popUpTableView.transform = CGAffineTransform(translationX: 0, y: popUpTableView.frame.height)
+        // popUpTableView 밑으로 숨긴 상태로 시작
+        popUpTableView.delegate = self
+        popUpTableView.dataSource = self
+        
+    }
+    
+    // MARK: Function Text Style
+    
+    private func setText() {
+        
         runButton.backgroundColor = UIColor.lightishBlue
         runButton.setTitle("RUN NOW", for: .normal)
         runButton.setTitleColor(.white, for: .normal)
         runButton.layer.cornerRadius = 12
-//        mentLabel1.font = UIFont(name: "NanumSquareB", size: 20)
-//        mentLabel2.font = UIFont(name: "NanumSquareB", size: 20)
-//        mentLabel3.font = UIFont(name: "NanumSquareB", size: 20)
         runButton.titleLabel?.font = UIFont(name: "AvenirNext-BoldItalic", size: 30)
-        popUpTableView.layer.cornerRadius = 20
-        //self.tabBarController?.tabBar.isHidden = false
         
-        //self.navigationController?.isNavigationBarHidden = true
-        self.navigationController?.navigationBar.setBackgroundImage(UIImage(), for: UIBarMetrics.default)
-        self.navigationController?.navigationBar.shadowImage = UIImage()
-        self.navigationController?.navigationBar.isTranslucent = true
-       //구분선 제거
-        //popUpTableView.separatorStyle = .none
-        popUpTableView.delegate = self
-        popUpTableView.dataSource = self
+        introLabel.font = UIFont(name: "NanumSquareB", size: 20)
+        introLabel.numberOfLines = 0
+        introLabel.text = "상쾌한 오전,\n당신의 러닝상대를\n찾아 함께 뛰어보세요"
         
-        if self.view.frame.height > 700 {
-            constraint_logo_ment.constant = 34
-            constraint_ment_homeImg.constant = 59
-            constarint_homeImg_button.constant = 51
-            
-        }
     }
-    
-    @objc func handleDismiss() {
-        popUpTableView.isHidden = true
-    }
-    
 }
+
+// MARK: UITableViewDataSource
 
 extension MainVC: UITableViewDataSource {
     
@@ -137,21 +159,34 @@ extension MainVC: UITableViewDataSource {
         
         return popUpCell
     }
-
+    
 }
 
+// MARK: UITableViewDelegate
+
 extension MainVC: UITableViewDelegate {
+    
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        // 셀 하나의 높이 값
+        
         return 90
     }
+    
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
         if indexPath == [0,0] {
-            //authorizeHealthKit()
-            guard let PopUpPush = self.storyboard?.instantiateViewController(identifier:"MatchingGoalVC") as? MatchingGoalVC else {return}
-            PopUpPush.hidesBottomBarWhenPushed = true
-            self.navigationController?.pushViewController(PopUpPush, animated: true)
-        } else {
-           // authorizeHealthKit()
+            // 다른 러너와 경쟁하기로 이동
+            
+            guard let nextVC = self.storyboard?.instantiateViewController(identifier:"MatchingGoalVC") as? MatchingGoalVC else { return }
+            nextVC.hidesBottomBarWhenPushed = true
+            // 이동 시 탭바 숨기기
+            
+            self.navigationController?.pushViewController(nextVC, animated: true)
+        }
+        
+        else {
+            // 나의 기록과 경쟁하기로 이동
+
         }
     }
     
