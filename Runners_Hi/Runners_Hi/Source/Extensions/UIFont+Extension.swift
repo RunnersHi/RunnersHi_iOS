8af496197ddc235b0ed90b2e87d703a083c9e207
xwoud <xwoud@naver.com>
2021-03-26 00:50:54 +0900

    [update] UIFont+Extension 완성
    body:  Custom 폰트를 쉽게 적용하는 Extension 제작
    
    Related to: #18

--- Runners_Hi/Runners_Hi/Source/Extensions/UIFont+Extension.swift
+++ Runners_Hi/Runners_Hi/Source/Extensions/UIFont+Extension.swift
@@ -7,3 +7,26 @@
 //
 
 import UIKit
+
+extension UIFont {
+    
+    // Custom 폰트를 쉽게 적용하는 Extension
+    // 사용법 : mentLabel.font = UIFont.nanumBold(size: 22)
+    
+    // MARK: NanumSquare Font
+    
+    class func nanumBold(size: CGFloat) -> UIFont {
+        return UIFont(name: "NanumSquareB", size: size)!
+    }
+    
+    class func nanumRegular(size: CGFloat) -> UIFont {
+        return UIFont(name: "NanumSquareR", size: size)!
+    }
+    
+    // MARK: AvenirNext BoldItalic Font (숫자용)
+    
+    class func numberFont(size: CGFloat) -> UIFont {
+        return UIFont(name: "AvenirNext-BoldItalic", size: size)!
+    }
+    
+}
