d1e088821921f72d949e80848911a8365b85494a
최영재 <brandon94_@naver.com>
2021-03-28 01:19:47 +0900

    [refactor] MyPageVc Refactoring4 body : MyPageVC CollectionView Cell 데이터 처리 및 세팅 부분 함수화
    Related to : #14 #17 #24

--- Runners_Hi/Runners_Hi/Source/Cell/ProfileBadgeCell.swift
+++ Runners_Hi/Runners_Hi/Source/Cell/ProfileBadgeCell.swift
@@ -22,4 +22,39 @@ class ProfileBadgeCell: UICollectionViewCell {
     func profilebadgeset(){
          myProfileName.font = UIFont(name: "NanumSquareB", size:12.0)
     }
+    
+    // 해당 셀에 데이터를 전달하고, 이에 맛게 뱃지를 표시하도록 하는 함수
+    func setMyProfileBadgeData(data: MyProfile?, index: Int){
+        // 달성하지 못한 배지의 이미지 이름 리스트
+        let myProfileBeforeBadgeImage = ["imgBadgeEggEmpty","imgBadgeChickEmpty","imgBadgeChickenEmpty","imgBadgeBatEmpty","imgBadgeBirdEmpty", "imgBadgeTurtleEmpty","imgBadge50Empty","imgBadge100Empty","imgBadge150Empty","imgBadgeStraightEmpty","imgBadgeSpeedEmpty","imgBadgeFlameEmpty"]
+        
+        // 달성한 배지의 이미지 이름 리스트
+        let myProfileBadgeImage = ["imgBadgeEgg","imgBadgeChick","imgBadgeChicken","imgBadgeBat","imgBadgeBird", "imgBadgeTurtle","imgBadge50","imgBadge100","imgBadge150","imgBadgeStraight","imgBadgeSpeed","imgBadgeFlame"]
+        
+        // 배지의 이름 리스트
+        let myProfileBadgeLabel = ["첫승달성","10승 달성", "50승 달성", "최고 페이스", "최장 거리", "최저 페이스", "50시간 달성", "100시간 달성", "150시간 달성", "10일 연속 러닝", "연속 5승", "연속 10승"]
+        
+        // 서버로부터 사용자의 뱃지 정보를 받아온다
+        let profileBadgeList = data?.result.badge ?? [true, false,false,false,false,false,false,false,false,false,false,false]
+        print("나오냐?", profileBadgeList, index)
+        
+        // 모든 배지의 이름을 맞춰서 넣어준다.
+        myProfileName.text = myProfileBadgeLabel[index]
+        
+        // 사용자로부터 받아온 배지 정보가 false 라면 달성하지 못한 배지로 여기고
+        // 달성하지 못한 배지의 이미지를 삽입한다
+        if profileBadgeList[index] == false {
+            myProfileBadge.image = UIImage(named: myProfileBeforeBadgeImage[index])
+            // false일 때는 item select 안되도록 설정
+            isUserInteractionEnabled = false
+        }
+        // 사용자로부터 받아온 배지 정보가 true인 경우
+        // 달성한 배지의 이미지를 삽입한다.
+        else {
+            myProfileBadge.image = UIImage(named: myProfileBadgeImage[index])
+            // True일 대는 item이 select 가능하도록 설정해준다
+            isUserInteractionEnabled = true
+        }
+
+    }
 }
--- Runners_Hi/Runners_Hi/Source/ViewController/MyPageVC.swift
+++ Runners_Hi/Runners_Hi/Source/ViewController/MyPageVC.swift
@@ -37,35 +37,9 @@ extension MyPageVC: UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         
         guard let ProfileBadgeCell = collectionView.dequeueReusableCell(withReuseIdentifier: ProfileBadgeCell.identifier, for: indexPath) as? ProfileBadgeCell else { return UICollectionViewCell()}
-        
-        // 달성하지 못한 배지의 이미지 이름 리스트
-        let myProfileBeforeBadgeImage = ["imgBadgeEggEmpty","imgBadgeChickEmpty","imgBadgeChickenEmpty","imgBadgeBatEmpty","imgBadgeBirdEmpty", "imgBadgeTurtleEmpty","imgBadge50Empty","imgBadge100Empty","imgBadge150Empty","imgBadgeStraightEmpty","imgBadgeSpeedEmpty","imgBadgeFlameEmpty"]
-        
-        // 달성한 배지의 이미지 이름 리스트
-        let myProfileBadgeImage = ["imgBadgeEgg","imgBadgeChick","imgBadgeChicken","imgBadgeBat","imgBadgeBird", "imgBadgeTurtle","imgBadge50","imgBadge100","imgBadge150","imgBadgeStraight","imgBadgeSpeed","imgBadgeFlame"]
-        
-        // 배지의 이름 리스트
-        let myProfileBadgeLabel = ["첫승달성","10승 달성", "50승 달성", "최고 페이스", "최장 거리", "최저 페이스", "50시간 달성", "100시간 달성", "150시간 달성", "10일 연속 러닝", "연속 5승", "연속 10승"]
-        
-        // 서버로부터 사용자의 뱃지 정보를 받아온다
-        let profileBadgeList = MyProfileModel?.result.badge
-        
-        // 사용자로부터 받아온 배지 정보가 false 라면 달성하지 못한 배지로 여기고
-        // 달성하지 못한 배지의 이미지를 삽입한다
-        if profileBadgeList?[indexPath.row] == false {
-            ProfileBadgeCell.myProfileBadge.image = UIImage(named: myProfileBeforeBadgeImage[indexPath.row])
-            // false일 때는 item select 안되도록 설정
-            ProfileBadgeCell.isUserInteractionEnabled = false
-        }
-        // 사용자로부터 받아온 배지 정보가 true인 경우
-        // 달성한 배지의 이미지를 삽입한다.
-        else {
-            ProfileBadgeCell.myProfileBadge.image = UIImage(named: myProfileBadgeImage[indexPath.row])
-            // True일 대는 item이 select 가능하도록 설정해준다
-            ProfileBadgeCell.isUserInteractionEnabled = true
-        }
-        // 모든 배지의 이름을 맞춰서 넣어준다.
-        ProfileBadgeCell.myProfileName.text = myProfileBadgeLabel[indexPath.row]
+       
+        // 해당 셀에 데이터를 전달하고, 데이터에 맞게 뱃지를 세팅하도록 하는 함수를 호출한다
+        ProfileBadgeCell.setMyProfileBadgeData(data: MyProfileModel, index: indexPath.row)
         
         return ProfileBadgeCell
     }
@@ -73,8 +47,6 @@ extension MyPageVC: UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
         switch kind {
         case UICollectionView.elementKindSectionHeader:
-            
-            
             
             guard let MyProfileHeader = collectionView.dequeueReusableSupplementaryView(ofKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: MyProfileHeader.identifier, for: indexPath) as? MyProfileHeader else { return UICollectionViewCell()}
             
@@ -134,10 +106,6 @@ extension MyPageVC {
                 let response = res as! MyProfile
                 self.MyProfileModel = response
                 self.myPageCollectionView.reloadData()
-                
-                // 오늘 하루종일 아무것도 못했는데, 그 이유를 여기서 찾았다.
-                // 바보같은 과거의 최영재. 과영재는 서버 통신에 성공했을 때에만 datasource,delegate를 self를 해주는 바보같은 과오를 저지르고 말았다.
-                // 하지만 난 이제 더 나아질 수 있다. 왜냐하면 이 문제를 발견했기 때문이다.
                 self.myPageCollectionView.dataSource = self
                 self.myPageCollectionView.delegate = self
                 
